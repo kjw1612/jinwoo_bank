0bc38e830dbdcf43259e9d30b026216e39560ae9
jiwnoo <jori3597@naver.com>
2023-06-01 14:42:19 +0900

    과제작성

--- Project4/cal.cpp
+++ Project4/cal.cpp
@@ -1,26 +1,78 @@
+#include<string>
+using namespace std;
+
 class Account
 {
 public:
-	explicit Account(int money) : balance(money)
+	explicit Account(int money) : balance_(money)
 	{
 
 	}
 
 	int getBalance()
 	{
-		return balance;
+		return balance_;
 	}
 
 	void deposit(int money)
 	{
-		balance += money;
+		balance_ += money;
 	}
 
 	void Withdraw(int money)
 	{
-		balance -= money;
+		balance_ -= money;
+	}
+
+	void SetInterest(int interest)
+	{
+		this->interest_ = interest;
+	}
+
+
+
+	void setBankInterest(string bank)
+	{
+		if(bank == "WOORI")
+		{
+			interest_ = 1;
+		}
+		else if(bank == "KB")
+		{
+			interest_ = 4;
+		}
+		else if(bank == "NH")
+		{
+			interest_ = 7;
+		}
+	}
+
+	int setCompoundInterset(void)
+	{
+		balance_ = balance_ * (1 + 0.005);
+	}
+
+	int GetBalanceByInterest(int balance)
+	{
+		return balance * (100 + interest_) / 100;
+	}
+
+	void ApplyInterest()
+	{
+		balance_ = GetBalanceByInterest(balance_);
+	}
+
+	int PredictPrice(int i)
+	{
+		int result = balance_;
+		for(int t = 0; t < i; t++)
+		{
+			result = GetBalanceByInterest(result);
+		}
+		return result;
 	}
 
 private:
-	int balance;
+	int balance_;
+	int interest_;
 };
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -20,3 +20,15 @@ TEST_F(AccountFixture, Withdraw) {
 	account.Withdraw(600);
 	EXPECT_EQ(9400, account.getBalance());
 }
+
+TEST_F(AccountFixture, InterrestFive) {
+	account.SetInterest(5);
+	account.ApplyInterest();
+	EXPECT_EQ(10500, account.getBalance());
+}
+
+TEST_F(AccountFixture, PredictPrice) {
+	account.SetInterest(10);
+	int ret = account.PredictPrice(3);
+	EXPECT_EQ(13310, ret);
+}
